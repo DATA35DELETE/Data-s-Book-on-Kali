51d3163d82a94bf693de473ebaeb6cd05c52a2d4
DATA <data.delete3509@gmail.com>
2026-01-31 15:52:31 +0300

    1.1v

---
+++ Sources/JS/setBackgroundMedia.js
@@ -0,0 +1,4 @@
+if(false)
+{
+    document.write("<div class='video-container'><video autoplay muted loop id='arkaplanVideo'><source src='../Media/video/STARFALL.mp4' type='video/mp4'>Tarayıcınız video oynatmayı desteklemiyor.</video></div>");
+}
